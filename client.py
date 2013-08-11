724d2f86fa7d770a3960ce4d5b5b354e8a310be2
ersran9 <sreenidimn@gmail.com>
2013-08-12 00:11:45 +0530

    working client, but far from perfect

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
-from twisted.internet import stdio, reactor
+from twisted.internet import stdio
+from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 
 
@@ -12,18 +13,19 @@ class CommandParser(object):
             if status == 'ERR':
                 return prompt.writeData(line.split(':', 1)[1], err=True)
             cmd = line.split(':', 2)[1]
-
-            self.OK_dispatch.get(cmd, self.errhandle)(protocol, prompt, line.split(':',3)[2])
+            print 'debug', cmd, ', ',line
+            self.OK_dispatch.get(cmd, self.errhandle)(protocol, prompt, line)
         except IndexError:
             self.errhandle(protocol, prompt)
 
     def set_nick(self, protocol, prompt, data):
-        protocol.nick = data
+        protocol.nick = data.split(':', 3)[2]
         prompt.writeData('Set nick to %s' %(protocol.nick))
 
-    def chat(self. protocol, prompt, data):
+    def chat(self, protocol, prompt, data):
         try:
-            nick, msg = data.split(':')
+            print 'data is', data
+            nick, msg = data.split(':', 3)[2], data.split(':', 3)[3]
         except ValueError:
             self.errhandle(protocol, prompt)
         prompt.writeData('< %s > %s' %(nick, msg))
@@ -63,7 +65,6 @@ class CommandPrompt(LineReceiver):
                     return self.factory.protocol_instance.sendLine('UNREGISTER:')
             except ValueError:
                 pass
-
         self.factory.protocol_instance.sendLine('CHAT:'+line)
 
     def writeData(self, line, err=None):
@@ -71,6 +72,7 @@ class CommandPrompt(LineReceiver):
             status = 'ERROR'
         else:
             status = 'OK'
+        print self
         self.sendLine('['+status+'] ' + line)
     
 
@@ -80,14 +82,14 @@ class ChatProtocol(LineReceiver):
         self.nick = None
 
     def connectionMade(self):
-        self.factory.prompt.writeData('Connected to server at: %s ', self.transport.getPeer())
+        self.factory.cmd.writeData('Connected to server at: %s ' %(self.transport.getPeer()))
 
     def connectionLost(self):
         if self.unreg:
             return
-        self.factory.prompt.writeData('Lost connection to server at: %s ', self.transport.getPeer()) 
+        self.factory.cmd.writeData('Lost connection to server at: %s ' %(self.transport.getPeer())) 
 
-    def LineReceived(self, line):
+    def lineReceived(self, line):
         self.factory.cmdparser.parseIncomingFromServer(self, self.factory.cmd, line)
 
 class ChatProtocolFactory(ClientFactory):
@@ -95,7 +97,7 @@ class ChatProtocolFactory(ClientFactory):
     protocol = ChatProtocol
 
     def __init__(self):
-        self.cmdparser = CommandLineParser()
+        self.cmdparser = CommandParser()
         self.cmd = CommandPrompt()
 
     def buildProtocol(self, addr):
@@ -113,8 +115,9 @@ def main():
     
     factory = ChatProtocolFactory()
     cmdprompt = CommandPrompt()
+    cmdprompt.factory = factory
     factory.cmd = cmdprompt
-    reactor.connectTCP(sys.argv[1], int(sys.argv[2]), ChatProtocolFactory())
+    reactor.connectTCP(sys.argv[1], int(sys.argv[2]), factory)
     stdio.StandardIO(cmdprompt)
     reactor.run()
 
